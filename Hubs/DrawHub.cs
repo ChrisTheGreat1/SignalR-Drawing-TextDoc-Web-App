f12981779862638aa2ee5f75ee3f9a882612bee1
Chris Brown <chrisbrown1@hotmail.ca>
2023-03-03 17:12:19 -0500

    Add project files.

---
+++ Hubs/DrawHub.cs
@@ -0,0 +1,29 @@
+﻿using DrawingApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Drawing;
+
+namespace DrawingApp.Hubs
+{
+    public class DrawHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("newMessage", message);
+        }
+
+        //public async Task NewStroke(DrawingPoint start, DrawingPoint end)
+        //{
+        //    await Clients.Others.SendAsync("newStroke", start, end);
+        //}
+
+        public async Task NewStroke(int x, int y)
+        {
+            await Clients.Others.SendAsync("newStroke", x, y);
+        }
+    }
+}
---
+++ Models/DrawingPoint.cs
@@ -0,0 +1,11 @@
+﻿namespace DrawingApp.Models
+{
+    public class DrawingPoint
+    {
+        //public int X { get; set; }
+
+        //public int Y { get; set; }
+
+        public int pos { get; set; }
+    }
+}
